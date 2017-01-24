a9061bd5cfc87d2f1c058eaaf64c245d29a8914d
ychao21 <changingtimes@gmail.com>
2017-01-23 21:14:00 -0600

    First Commit

---
+++ webpackLoader/src/index.js
@@ -0,0 +1,5 @@
+import greeting from './greeting';
+
+console.log('index.js loaded');
+
+document.body.appendChild(greeting());
---
+++ webpackLoader/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'build'),
+};
+
+module.exports = {
+  // Entry accepts a path or an object of entries.
+  // We'll be using the latter form given it's
+  // convenient with more complex configurations.
+  //
+  // Entries have to resolve to files! It relies on Node.js
+  // convention by default so if a directory contains *index.js*,
+  // it will resolve to that.
+  entry: {
+    src: PATHS.src,
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js',
+  }
+};
